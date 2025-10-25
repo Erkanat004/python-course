56b5324c8271be20eb475cf7ed8ea0ffe2e0124f
Erkanat004 <vvb099350@gmail.com>
2025-10-25 15:11:36 +0500

    Update Compiler.js

--- frontend/src/pages/Compiler.js
+++ frontend/src/pages/Compiler.js
@@ -34,7 +34,7 @@ print("Привет, мир!")
 
     try {
       // Отправляем код на наш бэкенд для выполнения
-     const response = await fetch('https://python-course-zg1y-dap4i8lay-erkanat004s-projects.vercel.app/api/compiler/execute', {
+     const response = await fetch('https://python-course-zg1y-git-main-erkanat004s-projects.vercel.app/api/compiler/execute', {
 
         method: 'POST',
         headers: {
